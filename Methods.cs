69d603677d68ee8f6b2231add82f730e299564e5
Yulia Vinogradova <149874841+Yuskus@users.noreply.github.com>
2024-05-27 14:55:39 +0300

    Update Methods.cs 14-55 27-05-24

--- Methods.cs
+++ Methods.cs
@@ -2,38 +2,36 @@ using UnityEngine.UI;
 
 public static class Methods
 {
-    public static decimal proc; //множитель процента
-    public static decimal upt, u; //бонус за юпт, множитель юпт
-    public static decimal bonus; //бонус
-    public static decimal[] uptProcent = new decimal[4] { 0, 10, 15, 20 }; //значения множителя юпт
+    public static decimal[] uptProcent = new decimal[4] { 0, 10, 15, 20 }; //Г§Г­Г Г·ГҐГ­ГЁГї Г¬Г­Г®Г¦ГЁГІГҐГ«Гї ГѕГЇГІ
+    
     public static decimal CountProcent(decimal plan)
     {
-        proc = 1;
-        if (plan >= 70 & plan < 90) { proc = 2; }
-        else if (plan >= 90 & plan < 95) { proc = 2.5M; }
-        else if (plan >= 95 & plan < 100) { proc = 2.7M; }
-        else if (plan >= 100) { proc = 3; }
-        return proc;
+        return plan switch
+        {
+            >= 100 => 3M,
+            >= 95 => 2.7M,
+            >= 90 => 2.5M,
+            >= 70  => 2M,
+            _ => 1M
+        };
     }
+    
     public static decimal CountBonus(decimal plan, decimal pm, decimal tom, decimal v)
     {
-        bonus = 0;
-        if (plan >= 105) { bonus = (tom - (pm * 1.05M)) * 0.05M * v; }
-        return bonus;
+        return plan >= 105 ? (tom - (pm * 1.05M)) * 0.05M * v : 0;
     }
+    
     public static decimal CountUPT(decimal plan, decimal hours, decimal uptProcent)
     {
-        upt = 0;
-        if (plan >= 80) { upt = hours * uptProcent; }
-        return upt;
+        return plan >= 80 ? hours * uptProcent : 0;
     }
+    
     public static decimal Toggles(Toggle[] toggle)
     {
-        u = 0;
         for (int i = 0; i < toggle.Length; i++)
         {
-            if (toggle[i].isOn) { u = uptProcent[i]; break; }
+            if (toggle[i].isOn) { return uptProcent[i]; }
         }
-        return u;
+        return 0;
     }
 }
